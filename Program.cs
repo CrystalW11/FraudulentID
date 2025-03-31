9e7e191092a1814117a2e9dacf45b504754ea35a
CrystalW11 <crystal.warmack@gmail.com>
2025-03-31 10:27:30 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] fraudulentOrdersIDs = new string[3];
+
+fraudulentOrdersIDs[0] = "A123";
+fraudulentOrdersIDs[1] = "B456";
+fraudulentOrdersIDs[2] = "C789";
+fraudulentOrdersIDs[3] = "D000";
